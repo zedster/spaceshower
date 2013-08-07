8f5843adc0fe0ab5f3602a9d98489856940c7034
zedster <s@birlet.com>
2013-08-07 20:57:52 +0100

    Initial addition to github, first time, in theory properly?

---
+++ SpaceShower/src/main/java/com/zstudios/spaceshower/Home.java
@@ -0,0 +1,36 @@
+package com.zstudios.spaceshower;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class Home extends Activity {
+
+    private static final int REQUEST_PATH = 1;
+    String curFileName;
+    EditText edittext;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.file_prompt);
+        edittext = (EditText)findViewById(R.id.editText);
+    }
+
+    public void getfile(View view){
+        Intent intent1 = new Intent(this, FileChooser.class);
+        startActivityForResult(intent1, REQUEST_PATH);
+    }
+    // Listen for results.
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        // See which child activity is calling us back.
+        if (requestCode == REQUEST_PATH){
+            if (resultCode == RESULT_OK) {
+                curFileName = data.getStringExtra("GetFileName");
+                edittext.setText(curFileName);
+            }
+        }
+    }
+}
